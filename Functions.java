76600ee5d820326ca6291c3ab0c7c19f34eab69a
MichealWilliamLR <100808920+MichealWilliamLR@users.noreply.github.com>
2022-05-04 03:03:51 +0800

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,4 +1,4 @@
-/** TODO: Replace all "..." into information(String)/ warnings(String)/ errors(String) */
+/** When ask for targetFile, please remember to use @targetFile.getParentFile() as the default combination for file accessing. */
 package com.MichealWilliam;
 
 import org.jetbrains.annotations.NotNull;
@@ -10,48 +10,14 @@ public class Functions {
 
 	public Scanner scn = new Scanner(System.in);
 	public File fileHomePath = new File( "/home/william" );
-	public File targetPath = new File(fileHomePath, "/Documents/StudyFiles" );
 	public String systemType = System.getProperty( "os.name" );
-	public File targetFile;
 	public Runtime runtime;
 	public Process process;
 
-	public boolean ifAsk(@NotNull File targetObject, String action) {
-		request("Would you like to " + action + " " + targetObject + " ? y/n" );
-		return scn.next().equalsIgnoreCase( "y" );
-	}
-	public void creating(String targetType, @NotNull File targetObject) throws IOException {
-		String action = "Creating";
-		String cmd = targetType.equalsIgnoreCase( "File" ) ? "touch " + targetObject : "mkdir " + targetObject;
-		// Ask
-		if ( ifAsk(targetObject, action) ) {
-			runtime = Runtime.getRuntime();
-			process = runtime.exec(cmd);
-		} else {
-			requestDenied(targetObject, targetType);
-		}
-	}
-	public void requestDenied(@NotNull File targetObject, String targetType) {
-		warnings(targetType + " " + targetObject + " failed, caused & canceled by user" );
-	}
-	public boolean checkExistence(@NotNull File targetObject) {
-		return targetObject.exists();
-	}
-	public void request(String content) {
-		System.out.println( "Request: " + content);
-	}
-	public void information(String content) {
-		System.out.println( "Info: " + content);
-	}
-	public void warnings(String content) {
-		System.out.println( "Warning: " + content);
-	}
-	public void errors(String content) {
-		System.out.println( "Error: " + content);
-	}
-
 	public static class TodoList extends Functions {
 
+		public File targetPath = new File(fileHomePath, "/Documents/StudyFiles" );
+		public File targetFile = new File(targetPath, "/TodoList.study");
 		public FileReader fileReader;
 		public FileWriter fileWriter;
 		public String targetFile_AbsolutePath;
@@ -59,7 +25,48 @@ public class Functions {
 		public ArrayList<Character> targetFileContent;
 		public static boolean isAnomalous = false;
 
-		public boolean Preparation(File targetPath) throws IOException {
+		public boolean ifAsk(@NotNull File targetObject, String action) {
+			request("Would you like to " + action + " " + targetObject + " ? y/n" );
+			return scn.next().equalsIgnoreCase( "y" );
+		}
+		public void creating(@NotNull File targetObject, String targetType) throws IOException {
+			String action = "Creating";
+			String cmd = targetType.equalsIgnoreCase( "File" ) ? "touch " + targetObject.getParentFile() : "mkdir " + targetObject;
+			// Ask
+			if ( ifAsk(targetObject, action) ) {
+				runtime = Runtime.getRuntime();
+				process = runtime.exec(cmd);
+			} else {
+				requestDenied(targetObject, targetType);
+			}
+		}
+		public void loading(@NotNull File targetObject, String targetType) {
+			if ( targetType.equalsIgnoreCase("File") ) {
+				targetFile = targetObject.getAbsoluteFile();
+			} else {
+				targetPath = targetObject.getAbsoluteFile();
+			}
+		}
+		public void requestDenied(@NotNull File targetObject, String targetType) {
+			warnings(targetType + " " + targetObject + " failed, caused & canceled by user" );
+		}
+		public boolean checkExistence(@NotNull File targetObject, String targetType) {
+			return targetType.equalsIgnoreCase("File") ? targetObject.getParentFile().exists() : targetObject.exists();
+		}
+		public void request(String content) {
+			System.out.print( "Request: " + content);
+		}
+		public void information(String content) {
+			System.out.println( "Info: " + content);
+		}
+		public void warnings(String content) {
+			System.out.println( "Warning: " + content);
+		}
+		public void errors(String content) {
+			System.out.println( "Error: " + content);
+		}
+		public boolean Preparation(File targetFile) throws IOException {
+			/** Why does targetFile path cannot be specified into a file-unit? */
 
 			// Check existence of the path
 				// Not:
@@ -74,53 +81,51 @@ public class Functions {
 				// Aim file
 				// isAnomalous = false;
 
-			// TODO: loadPath(File targetPath)
-
-			if( !checkExistence(targetPath) ) {
-				warnings("Your target path does not seem to be existed" );
-				// Auto doing, no ask
-				creating( "path", targetPath);
+			if( !checkExistence(targetPath, "path") ) {
 				isAnomalous = true;
+				warnings("Target path does not seem to be existed" );
+				// Auto doing, no asks
+				creating( targetPath, "path");
 				// Retry
-				Preparation(targetPath);
+				Preparation(targetFile);
 			} else {
+				isAnomalous = false;
 				information("Target path " + targetPath + " exists" );
 				// Judge whether the targetFile exists or not
-				if ( !checkExistence(targetFile) ) {
-					warnings("Your target file does not seem to be existed" );
+				if ( !checkExistence(this.targetFile, "file") ) {
+					isAnomalous = true;
+					warnings("Target file does not seem to be existed" );
 					// Auto doing, no ask
-					creating( "file", targetFile);
+					creating(targetFile, "file");
 					// Retry
 					Preparation(targetPath);
+				} else {
+					// targetFile exists
+					isAnomalous = false;
+					information("Target file " + targetFile + " exists");
 				}
 			}
 
-			return true;
+			return !isAnomalous;
 		}
-
-		public boolean onCreate(File targetFile) throws IOException {
+		public boolean onCreate() throws IOException {
 
 			targetFileContent = new ArrayList<>(0);
-			super.targetFile = new File(super.targetPath, "/TODO.study" ); // TodoList
-
-			// Special situation (theoretically impossible)
-			if ( !Preparation(super.targetPath) ) {
-				errors( "Preparation(File) was not prepared properly" );
-				isAnomalous = true;
-			}
 
 			// Use @isAnomalous to judge whether onCreate(File) is anomalous
-			if ( isAnomalous ) {
+			if ( !Preparation(targetPath) ) {
+				errors( "Preparation(File) was not prepared properly" );
+				isAnomalous = true;
 				return false;
 			}
 
 			// The targetPath & targetFile has been confirmed
 			// Load AbsolutePath & CanonicalPath
-			targetFile_AbsolutePath = super.targetFile.getAbsolutePath();
-			targetFile_CanonicalPath = super.targetFile.getCanonicalPath();
+			targetFile_AbsolutePath = targetFile.getParentFile().getAbsolutePath();
+			targetFile_CanonicalPath = targetFile.getParentFile().getCanonicalPath();
 
-			fileReader = new FileReader(super.targetFile);
-			fileWriter = new FileWriter(super.targetFile);
+			fileReader = new FileReader(this.targetFile);
+			fileWriter = new FileWriter(this.targetFile);
 			targetFileContent.add( (char)fileReader.read() );
 
 			// Test
