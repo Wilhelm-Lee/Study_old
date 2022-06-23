4a4332c990b8cdde53eab30255c9680ff7648b52
MichealWilliamLR <100808920+MichealWilliamLR@users.noreply.github.com>
2022-06-23 11:24:53 +0800

    Update Settings.java

--- Settings.java
+++ Settings.java
@@ -8,13 +8,13 @@ public class Settings {
 		// greetings(String)
 		LocalDateTime localDateTime = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yy|HH:mm:ss");
-	static final String BLACK = "0";
-	static final String RED = "1";
-	static final String GREEN = "2";
-	static final String YELLOW = "3";
-	static final String BLUE = "4";
-	static final String MAGENTA = "5";
-	static final String CYAN = "6";
-	static final String WHITE = "7";
-	static final String RESET = "8"; // Default to be FRONT - BLACK
+	static final char BLACK = '0';
+	static final char RED = '1';
+	static final char GREEN = '2';
+	static final char YELLOW = '3';
+	static final char BLUE = '4';
+	static final char MAGENTA = '5';
+	static final char CYAN = '6';
+	static final char WHITE = '7';
+	static final char RESET = '8'; // Default to be FRONT - BLACK
 }
