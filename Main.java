bcb06195dec03b59a905011c91be284d3c9a9c70
MichealWilliamLR <100808920+MichealWilliamLR@users.noreply.github.com>
2022-06-23 11:24:30 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,20 @@
 package com.michealwilliam;
 
-import java.io.IOException;
-
 /**
  * @author william
  */
+
 public class Main {
 
 	/**
 		These are the classes/methods witch need
-		to create realizations (Non-static):
+		to create realizations (AKA Non-static):
 			1. Functions.TodoList				[class]
 			2. BasicOutput.greetings(String)	[method]
 	 */
 
-	public static void main(String[] args) throws IOException {
-
-//		Functions functions = new Functions();
-//		Settings settings = new Settings();
-		BasicOutput bo = new BasicOutput();
-
-		// Permanent code
+	public static void main(String[] args) {
 		Functions.TodoList todoList = new Functions.TodoList();
-		// Permanent code OVER
-
-//		BasicOutput.information("Study", todoList.onCreate());
-		FileManager fm = new FileManager();
-		todoList.onCreate();
-
+		BasicOutput.log(BasicOutput.INFO, "Study.TodoList", String.valueOf(!todoList.onCreate()));
 	}
 }
