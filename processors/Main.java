d1634e1d63fe17bb556a0a0fdfa2a9858b397c3d
MWLR <michealwilliamlr@outlooc.com>
2022-08-07 03:55:31 +0800

    Update:     Mistake fixed in Main.java

--- processors/Main.java
+++ processors/Main.java
@@ -26,10 +26,10 @@ package com.study.processors;
  * | <!>unsigned	short			  	  0 to 2^32 - 1					 	  		 	  0 to 4294967295				0					2 Byte / 16 Bits						|
  * |	signed		int				  -2^32 to 2^32 - 1						    -2147483648 to 2147483647				0					4 Byte / 32 Bits						|
  * | <!>unsigned	int				  	  0 to 2^64 - 1						    		  0 to 9223372036854775807		0					4 Byte / 32 Bits						|
- * | <!>unsigned	float			 2^-149 to (2-2^-23)·2^127			   0x1.effeteP+127f	to 0x0.000002P-126f			0.0f				4 Byte / 32 Bits	Standard: IEEE 754	|
+ * |	unsigned	float			 2^-149 to (2-2^-23)·2^127			   0x1.effeteP+127f	to 0x0.000002P-126f			0.0f				4 Byte / 32 Bits	Standard: IEEE 754	|
  * |	signed		long			  -2^64 to 2^64 - 1				   -9223372036854775808 to 9223372036854775807		0					8 Byte / 64 Bits						|
  * | <!>unsigned	long			  	  0 to 2^64 - 1				   -9223372036854775808 to 9223372036854775807		0					8 Byte / 64 Bits						|
- * | <!>unsigned	double			2^-1074 to (2-2^-52)·2^1023		0x0.0000000000001P-1022 to 0x1.fffffffffffffP+1023	0.0d				8 Byte / 64 Bits	Standard: IEEE 754	|
+ * |	unsigned	double			2^-1074 to (2-2^-52)·2^1023		0x0.0000000000001P-1022 to 0x1.fffffffffffffP+1023	0.0d				8 Byte / 64 Bits	Standard: IEEE 754	|
  * *--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*
  * <!> Stands for Theoretically only | Not contained in Java
  * .............................................................................................................................................................August 7th, 2022.....
